d443aa3be3c164f17c9a2610a7e41c07c1370276
Cleanderson Lobo <cleandersonlobo@gmail.com>
2019-05-19 16:24:31 -0300

    Add Initial Actions and Properties in Redux

--- src/store/models/toolsModel/toolsModel.js
+++ src/store/models/toolsModel/toolsModel.js
@@ -1,6 +1,42 @@
-export const INITIAL_STATE = {};
+import api from '../../../api/tools';
+
+export const INITIAL_STATE = {
+  isFetching: false,
+  error: null,
+  tools: null,
+};
 
 export const toolsModel = {
   state: INITIAL_STATE,
-  reducers: {},
+  reducers: {
+    request: state => ({
+      ...state,
+      isFetching: true,
+      error: null,
+    }),
+    responseSuccess: (state, props) => ({
+      ...state,
+      isFetching: false,
+      ...props,
+    }),
+    requestFailure: state => ({
+      ...state,
+      isFetching: false,
+      error: true,
+    }),
+  },
+  effects: dispatch => ({
+    async getTools() {
+      try {
+        dispatch.toolsModel.request();
+        const res = await api.get();
+        if (res.status !== 200) throw new Error(res);
+        dispatch.toolsModel.responseSuccess({
+          tools: res.data,
+        });
+      } catch (error) {
+        dispatch.toolsModel.requestFailure();
+      }
+    },
+  }),
 };
